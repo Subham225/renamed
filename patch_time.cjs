269f8dc84d0e730158e4c5bffa0d7b558ccb991d
Subham225 <subhamdiscord09@gmail.com>
2026-07-25 14:10:36 +0530

    rzr

--- patch_time.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/CartDrawer.tsx', 'utf8');
-
-code = code.replace(/const isOpenNow = currentHour >= 0 && currentHour < 20;/g, "const isOpenNow = currentHour >= 0 && currentHour <= 24;");
-
-fs.writeFileSync('src/components/CartDrawer.tsx', code);
