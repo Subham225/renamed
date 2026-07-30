6a78bbddb23ea9b9499e83301e984fedb7815095
Subham225 <subhamdiscord09@gmail.com>
2026-07-30 14:46:15 +0530

    g

--- patch_category.sh
+++
@@ -1,1 +0,0 @@
-cat src/components/CategoryDetailPage.tsx | grep -n "return (" -A 150 > current_render.txt
--- patch_engines.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-pkg.engines = { node: ">=22.12.0" };
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));
